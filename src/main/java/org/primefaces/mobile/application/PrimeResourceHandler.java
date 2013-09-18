14331d7ce4e86492e27dcf7da09953aa157d287a
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-09-18 09:49:36 +0000

    Mobile refactor
    git-svn-id: http://primefaces.googlecode.com/svn/mobile/trunk@10053 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/mobile/application/PrimeResourceHandler.java
+++ src/main/java/org/primefaces/mobile/application/PrimeResourceHandler.java
@@ -15,27 +15,13 @@
  */
 package org.primefaces.mobile.application;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.el.ELContext;
-import javax.el.ValueExpression;
 import javax.faces.application.Resource;
 import javax.faces.application.ResourceHandler;
 import javax.faces.application.ResourceHandlerWrapper;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import org.primefaces.context.RequestContext;
-import org.primefaces.model.StreamedContent;
 import org.primefaces.mobile.util.Constants;
-import org.primefaces.util.StringEncrypter;
 
 public class PrimeResourceHandler extends ResourceHandlerWrapper {
-    
-    private final static Logger logger = Logger.getLogger(PrimeResourceHandler.class.getName());
-    
+        
     private ResourceHandler wrapped;
 
     public PrimeResourceHandler(ResourceHandler wrapped) {
@@ -58,66 +44,4 @@ public class PrimeResourceHandler extends ResourceHandlerWrapper {
             return resource;
         }
     }
-    
-    @Override
-    public void handleResourceRequest(FacesContext context) throws IOException {
-        Map<String,String> params = context.getExternalContext().getRequestParameterMap();
-        String library = params.get("ln");
-        String dynamicContentId = (String) params.get(org.primefaces.util.Constants.DYNAMIC_CONTENT_PARAM);
-        String nocache = params.get(org.primefaces.util.Constants.DYNAMIC_CONTENT_NOCACHE_PARAM);
-        StringEncrypter strEn = RequestContext.getCurrentInstance().getEncrypter();
-        
-        if(dynamicContentId != null && library != null && library.equals(Constants.LIBRARY)) {
-            StreamedContent streamedContent = null;
-            
-            try {
-                String dynamicContentEL = strEn.decrypt(dynamicContentId);                
-                ExternalContext externalContext = context.getExternalContext();
-                 
-                if(dynamicContentEL != null) {
-                    ELContext eLContext = context.getELContext();
-                    ValueExpression ve = context.getApplication().getExpressionFactory().createValueExpression(context.getELContext(), dynamicContentEL, StreamedContent.class);
-                    streamedContent = (StreamedContent) ve.getValue(eLContext);
-
-                    externalContext.setResponseStatus(200);
-                    externalContext.setResponseContentType(streamedContent.getContentType());
-                    
-                    if(nocache == null) {
-                        externalContext.setResponseHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-                        externalContext.setResponseHeader("Pragma", "no-cache");
-                        externalContext.setResponseHeader("Expires", "Mon, 8 Aug 1980 10:00:00 GMT");
-                    }
-                    
-                    if(streamedContent.getContentEncoding() != null) {
-                        externalContext.setResponseHeader("Content-Encoding", streamedContent.getContentEncoding());
-                    }
-
-                    byte[] buffer = new byte[2048];
-
-                    int length;
-                    InputStream inputStream = streamedContent.getStream();
-                    while ((length = (inputStream.read(buffer))) >= 0) {
-                        externalContext.getResponseOutputStream().write(buffer, 0, length);
-                    }
-                }
-
-                externalContext.responseFlushBuffer();
-                context.responseComplete();
-
-            } catch(Exception e) {
-                logger.log(Level.SEVERE, "Error in streaming dynamic resource. {0}", new Object[]{e.getMessage()});
-                throw new IOException(e);
-            }
-            finally {
-                //cleanup
-                if(streamedContent != null) {
-                    streamedContent.getStream().close();
-                }
-            }
-        }
-        else {
-           super.handleResourceRequest(context); 
-        }
-    }
 }
-
